13f6be344642789086729897a414efde9130e64d
HareWH <a411499275wang@qq.com>
2017-08-05 14:12:02 +0800

    Initial commit

---
+++ src/co/bdqn/tangcco/tcmp056/entity/Classes.java
@@ -0,0 +1,26 @@
+package co.bdqn.tangcco.tcmp056.entity;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class Classes {
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    private  int cid;
+    private String cname;
+
+}
---
+++ src/co/bdqn/tangcco/tcmp056/test.java
@@ -0,0 +1,21 @@
+package co.bdqn.tangcco.tcmp056;
+
+/**
+ * Created by Administrator on 2017/8/4.
+ */
+public class test {
+    public static void main(String[] args) {
+        int c = 0;
+        for (c = 0; c <= 10; c++) {
+            System.out.println("c=" + c);
+        }
+        int i = 5;
+        if (i > 5) {
+            System.out.println("i大于5");
+        } else if (i == 5) {
+            System.out.println("i等于5");
+        } else if (i < 5) {
+            System.out.println("i小于5");
+        }
+    }
+}
